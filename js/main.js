58ed45df6146ad74d65db1469e2ea924bd61b5db
NovakVladimir <vladimir.novak.89@i.ua>
2020-11-13 21:42:23 +0200

    Finish project

---
+++ js/main.js
@@ -0,0 +1,18 @@
+
+function isPalindrome() {
+    let usersWord = document.getElementById("palindrome").value;
+    let result = document.getElementById("message");
+
+    if(usersWord.includes(" ")) {
+        return(result.innerHTML = "You enter more than one word or number");
+    }
+
+    for(let i = 0; i <= usersWord/2; i++) {
+        if(usersWord[i] !== usersWord[usersWord.length - 1 - i]) {
+            return(result.innerHTML = "It isn't a palendrome");
+        }
+    }
+    result.style.backgroundColor = "green";
+    return(result.innerHTML = "Yes. It is a palendrome"); 
+
+}
